4a147172cbad2d00c51d8b40f504f5815e990780
your-github-username <stachtode1995@gmail.com>
2025-11-05 17:08:34 +0530

    commit

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="medical_chatbot",
     version="0.1.0",
-    author="Saagaar Naaamdev Taachtode",
-    author_email="sagar.tachtode.nitie.2020@gmail.com",
+    author="Boktiar Ahmed Bappy",
+    author_email="entbappy73@gmail.com",
     packages=find_packages(),
     install_requires=[]
 )
