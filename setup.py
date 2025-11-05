5836e3dd7bea3ec19d75a56eab4c66c941ba90c3
your-github-username <stachtode1995@gmail.com>
2025-11-05 15:36:19 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Saagaar Naaamdev Taachtode",
+    author_email="sagar.tachtode.nitie.2020@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
